d6117a74cb6d2cc3f5e49bb24847115af7a77373
Mentoons Developer <mentoonsdeveloper.ansari@gmail.com>
2022-10-07 17:31:22 +0530

    mm2

---
+++ index.js
@@ -0,0 +1,22 @@
+// SLIDER START
+
+// var indexValue = 0;
+// function slideShow() {
+//   setTimeout(slideShow, 2500);
+//   var x;
+//   const img = document.querySelectorAll("#slide");
+//   for (x = 0; x < img.length; x++) {
+//     slide[x].style.display = "none";
+//   }
+//   indexValue++;
+//   if (indexValue > img.length) {
+//     indexValue = 1;
+//   }
+//   slide[indexValue - 1].style.display = "block";
+// }
+// slideShow();
+
+// SLIDER END
+function myFunc() {
+  console.log("hello");
+}
