424a774182be418a03101d22246079897b93bef1
Mentoons Developer <mentoonsdeveloper.ansari@gmail.com>
2022-10-08 16:33:16 +0530

    updating1

--- index.js
+++ index.js
@@ -20,3 +20,40 @@
 function myFunc() {
   console.log("hello");
 }
+
+// ////////////////////////////
+/*
+ * This Below Code is intented for demo purpose only.
+ * Function initializeSlider runs the slider confined with above html format
+ * @param selector - required parameter - the target css id of the slider should be passed to this parameter
+ * @param interval - optional parameter - This can be used to control the interval between transition
+ * @param interval - optional parameter - This can be used to control the  time for transition
+ *
+ */
+
+function initializeSlider(selector, interval = 2000, transitionDuration = 3) {
+  const slider = document.getElementById(selector);
+  const sliderWrapper = slider.querySelector(".slider-container");
+  let direction;
+
+  sliderWrapper.addEventListener("transitionend", function () {
+    sliderWrapper.appendChild(sliderWrapper.firstElementChild);
+    sliderWrapper.style.transition = "none";
+    sliderWrapper.style.transform = "translate(0)";
+    setTimeout(() => {
+      sliderWrapper.style.transition = `all ${transitionDuration}s`;
+    });
+  });
+
+  // Runs The slider in cyclic manner
+  setInterval(() => {
+    direction = -1;
+    slider.style.justifyContent = "flex-start";
+    sliderWrapper.style.transform = "translate(-25%)"; // moves the slides
+  }, interval);
+}
+
+initializeSlider("slider-one");
+initializeSlider("slider-two");
+initializeSlider("slider-three");
+initializeSlider("slider-four");
